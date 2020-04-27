eca43bbf3ade676fb9cdfa4d58bbc55c29af43ba
Edunno <tamericandream@hotmail.com>
2020-04-27 15:16:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-var mysqlssh = require('mysql-ssh');
+const mysqlssh = require('mysql-ssh');
+const fs = require('fs');
+
+mysqlssh.connect({
+    host: '134.122.75.249',
+    user: 'root',
+    privateKey: fs.readFileSync(process.env.home + '/.ssh/New1')
+},
+{
+    host: '0.0.0.0:3306',
+    user: 'Bobl',
+    password: 'DeltaHotel11',
+    database: 'EventDB'
+}
+)
+.then(client => {
+    client.query('SELECT * FROM `users`', function (err, results, fields) {
+        if (err) throw err
+        console.log(results);
+        mysqlssh.close()
+    })
+})
+.catch(err => {
+    console.log(err)
+})
