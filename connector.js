bf94308cde6c301cb5297381bcfe16054360458f
Edunno <tamericandream@hotmail.com>
2020-04-29 13:53:15 +0200

    Moved mysql to connector, API started

---
+++ connector.js
@@ -0,0 +1,31 @@
+const mysqlssh = require('mysql-ssh');
+const fs = require('fs');
+const homedir = require('os').homedir;
+
+console.log('Starting mysqlssh.');
+mysqlssh.connect({
+    host: '134.122.75.249',
+    user: 'root',
+    privateKey: fs.readFileSync(homedir+'/../home/.ssh/New1')
+},
+{
+    host: '0.0.0.0',
+    port: '3306',
+    user: 'Bobl',
+    password: 'DeltaHotel11',
+    database: 'EventDB'
+}
+)
+.then(client => {
+    console.log('Connecting...');
+    client.query('SELECT * FROM `Event`', function (err, results, fields) {
+        if (err) throw err
+        console.log(results);
+        console.log('Success');
+        mysqlssh.close()
+    })
+})
+.catch(err => {
+    console.log(err)
+    console.log('Didn\'t work');
+})
--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-const mysqlssh = require('mysql-ssh');
-const fs = require('fs');
-const homedir = require('os').homedir;
+const express = require('extpress');
+const bodyparser = require('body-parser');
 
-console.log('Starting mysqlssh.');
-mysqlssh.connect({
-    host: '134.122.75.249',
-    user: 'root',
-    privateKey: fs.readFileSync(homedir+'/../home/.ssh/New1')
-},
-{
-    host: '0.0.0.0',
-    port: '3306',
-    user: 'Bobl',
-    password: 'DeltaHotel11',
-    database: 'EventDB'
-}
-)
-.then(client => {
-    console.log('Connecting...');
-    client.query('SELECT * FROM `Event`', function (err, results, fields) {
-        if (err) throw err
-        console.log(results);
-        console.log('Success');
-        mysqlssh.close()
-    })
-})
-.catch(err => {
-    console.log(err)
-    console.log('Didn\'t work');
-})
+const port = process.env.port || 8080;
+
+var app = express();
+app.use(bodyparser.json());
+
+app.listen(port, () => console.log('Listening on port ${port}..'));
