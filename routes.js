7a7087a96f9db9bb613a5442f883c5473f012011
Edunno <tamericandream@hotmail.com>
2020-04-30 13:54:43 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,6 +4,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/getEvents',controller.getEvents);
+router.get('/getEvents', true ,controller.getEvents);
 
 module.exports = router;
