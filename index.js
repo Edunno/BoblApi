8369cd0114d05643f5141458d6cc79f9505497cc
Edunno <tamericandream@hotmail.com>
2020-04-30 11:06:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ app.use(bodyparser.json());
 app.listen(port, () => {
     console.log('Listening on port ' + port);
 })
-
-app.get('/Event', (req, res) => {
-    var data = await conn.getSomeData();
-    res.send(data);
-    console.log('Data send:\n' + result);
-})
+async function sendQuery() {
+    app.get('/Event', (req, res) => {
+        var data = await conn.getSomeData();
+        res.send(data);
+        console.log('Data send:\n' + result);
+    })
+}
+sendQuery();
 
 
 /*
