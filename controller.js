c155685bf07260d33097308f65fd58e18f49ea3a
Edunno <tamericandream@hotmail.com>
2020-05-06 11:52:26 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -18,7 +18,7 @@ const getEvents = async (req, res, next) => {
 
 const createEvent = async (req, res, next) => {
   const pool = await mysqlssh.connect(conn.sshConfig, conn.dbConfig);
-  const results = await pool.query('INSERT INTO Event (title, adress, start_time) VALUES (\''+ req.event.title +'\', \'' + req.event.adress + '\', \'' + req.event.start_time + '\');', function(err , results, fields) {
+  const results = await pool.query('INSERT INTO Event (title, adress, start_time) VALUES (\'Træn\', \'Bornholm 80\', \'202020202\');', function(err , results, fields) {
     if (err) throw err;
     res.send(results)
   })
