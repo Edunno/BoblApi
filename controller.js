454f65a713ca054536a63bb88d4a85c62c0d9f59
Edunno <tamericandream@hotmail.com>
2020-05-13 11:36:25 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -193,27 +193,27 @@ const createEvent = async (req, res, next) => {
   let sqlStart = 'INSERT INTO `Event` (title, adress, start_day, start_time';
   let sqlEnd = ') VALUES (?,?,?,?';
   conditions.push(req.body.title);
-  conditions.push(req.body.adress);
+  conditions.push(req.body.address);
   conditions.push(req.body.startday);
   conditions.push(req.body.starttime);
 
   if (typeof req.body.endtime !== 'undefined') {
-    sqlStart += ',end_time';
+    sqlStart += ', end_time';
     sqlEnd += ',?';
     conditions.push(req.body.endtime);
   }
   if (typeof req.body.description !== 'undefined') {
-    sqlStart += ',description';
+    sqlStart += ', description';
     sqlEnd += ',?';
     conditions.push(req.body.description);
   }
   if (typeof req.body.place !== 'undefined') {
-    sqlStart += ',place';
+    sqlStart += ', place';
     sqlEnd += ',?';
     conditions.push(req.body.place);
   }
   if (typeof req.body.organizerid !== 'undefined') {
-    sqlStart += ',organizer_id';
+    sqlStart += ', organizer_id';
     sqlEnd += ',?';
     conditions.push(req.body.organizerid);
   }
